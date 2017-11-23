edd47357148bbd303c14d29831f7ec77a3f501bc
Cory LePage <cory@lepagegroup.ca>
2017-11-23 14:46:18 -0800

    Initial commit

--- CreateBeam/Form1.Designer.cs
+++ CreateBeam/Form1.Designer.cs
@@ -28,12 +28,55 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.pictureBox4 = new System.Windows.Forms.PictureBox();
+			this.CreateBeam2 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox4
+			// 
+			this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+			this.pictureBox4.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox4.Name = "pictureBox4";
+			this.pictureBox4.Size = new System.Drawing.Size(287, 62);
+			this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+			this.pictureBox4.TabIndex = 0;
+			this.pictureBox4.TabStop = false;
+			// 
+			// CreateBeam2
+			// 
+			this.CreateBeam2.BackColor = System.Drawing.Color.PowderBlue;
+			this.CreateBeam2.Location = new System.Drawing.Point(105, 101);
+			this.CreateBeam2.Name = "CreateBeam2";
+			this.CreateBeam2.Size = new System.Drawing.Size(112, 49);
+			this.CreateBeam2.TabIndex = 1;
+			this.CreateBeam2.Text = "Create Beam";
+			this.CreateBeam2.UseVisualStyleBackColor = false;
+			this.CreateBeam2.Click += new System.EventHandler(this.CreateBeam2_Click);
+			// 
+			// Form1
+			// 
+			this.BackColor = System.Drawing.SystemColors.Window;
+			this.ClientSize = new System.Drawing.Size(311, 191);
+			this.Controls.Add(this.CreateBeam2);
+			this.Controls.Add(this.pictureBox4);
+			this.Name = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button btn_CreateBeam;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button btnCreateBeam;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.PictureBox pictureBox4;
+		private System.Windows.Forms.Button CreateBeam2;
 	}
 }
 
--- CreateBeam/Form1.cs
+++ CreateBeam/Form1.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tekla.Structures.Model;
+using Tekla.Structures.Geometry3d;
 
 namespace CreateBeam
 {
@@ -15,6 +10,35 @@ namespace CreateBeam
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void CreateBeam2_Click(object sender, EventArgs e)
+		{
+			var model = new Model();
+
+			var point = new Point(0, 3000, 0);
+			var point2 = new Point(6000, 3000, 0);
+			var profile = new Profile { ProfileString = "W16X50" };
+			var material = new Material { MaterialString = "300W" };
+			var finish = "NO PAINT";
+			var theClass = "4";
+
+			var beam = new Beam
+			{
+				StartPoint = point,
+				EndPoint = point2,
+				Profile = profile,
+				Finish = finish,
+				Class = theClass,
+				Material = material,
+				StartPointOffset = new Offset(),
+				EndPointOffset = new Offset()
+			};
+
+			var success = beam.Insert();
+
+			if (success)
+				model.CommitChanges();
 		}
 	}
 }
